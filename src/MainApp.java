b058e8ad957f5db546185f15904201b07407a6ce
mario688 <gliwamario@gmail.com>
2020-11-25 00:24:28 +0100

    Extra Version with menu

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,5 @@
-
+import javax.xml.crypto.Data;
+import java.util.Scanner;
 
 //Author:Mario Gliwa, Karol Skwierawski
 public class MainApp {
@@ -8,10 +9,39 @@ public class MainApp {
         ConvertToMonths convertToMonths = new ConvertToMonths();
         CountNumberOfBreakdowns countNumberOfBreakdowns = new CountNumberOfBreakdowns();
 
-        failureRateCalculator.CalculateFailure();
-        convertToMonths.Convert(failureRateCalculator);
-        countNumberOfBreakdowns.CountBreakdowns();
+        int swValue;
+
+        // Display menu graphics
+        System.out.println(TextColor.BLUE + "=====================================================");
+        System.out.println("|         BSI-Project-Calculator                    |");
+        System.out.println("=====================================================");
+        System.out.println("| Options:                                          |");
+        System.out.println("|        1. failure Rate Calculator                 |");
+        System.out.println("|        2. failure Rate Calculator with convert    |");
+        System.out.println("|        3. count number of breakdowns              |");
+        System.out.println("|        4. EXIT                                    |");
+        System.out.println("=====================================================");
+        swValue = Keyin.inInt(TextColor.GREEN + " Select option: ");
+
+        // Switch construct
+        switch (swValue) {
+            case 1:
+                failureRateCalculator.CalculateFailure();
+                break;
+            case 2:
+                failureRateCalculator.CalculateFailure();
+                convertToMonths.Convert(failureRateCalculator);
+                break;
+            case 3:
+                countNumberOfBreakdowns.CountBreakdowns();
+                break;
+            default:
+                System.out.println("Invalid selection");
+                break; // This break is not really necessary
+        }
 
 
     }
+
 }
+
